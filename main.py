2f7b61e273d3a64dd6e6985463a0063c83737b01
5late <5late@users.noreply.github.com>
2021-06-20 16:37:58 +0000

    Automatic Code Fixes!

--- main.py
+++ main.py
@@ -2136,13 +2136,17 @@ async def shop(ctx):
     await ctx.send('Please send your PASSWORD now.')
     msg2 = await bot.wait_for("message", check=check(ctx.author), timeout=30)
     new_password = msg2.content
-    shop = checkshop.check_item_shop(username=new_username, password=new_password)
-    shop_embed = discord.Embed(title=f'Shop for {new_username}', description='Here is your shop.')
+    shop = checkshop.check_item_shop(
+        username=new_username,
+        password=new_password)
+    shop_embed = discord.Embed(
+        title=f'Shop for {new_username}',
+        description='Here is your shop.')
     for item in shop:
         if isinstance(item, str):
-            shop_embed.add_field(name='In your SHOP', value = item)
+            shop_embed.add_field(name='In your SHOP', value=item)
 
-    await ctx.send(embed = shop_embed)
+    await ctx.send(embed=shop_embed)
 
 if __name__ == "__main__":
     bot.loop.create_task(background_task())
