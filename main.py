21c80694387ef34f3070def9676574767a38ac89
5late <christianrfernandes5@gmail.com>
2021-08-16 00:02:47 -0400

    changes

--- main.py
+++ main.py
@@ -436,6 +436,8 @@ def checkStatusCode(status: str, task: str):
 @commands.cooldown(1, 16, commands.BucketType.user)
 @bot.command()
 async def val(ctx, *, arg: str):
+    if ctx.author.id == 323269361232248832:
+        return
     async def mainValFetch(arg):
         newArg = arg.split("#")
 
